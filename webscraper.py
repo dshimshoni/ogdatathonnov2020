54d8b87c4feddbcf2cdb0568842ccd197456e67a
Keerthana <63519382+keerthana-github@users.noreply.github.com>
2020-11-21 16:45:08 -0500

    webscraper final

--- webscraper.py
+++ webscraper.py
@@ -24,52 +24,59 @@ except ImportError:
 #set up path enviroment
 path='/Users/keerthanamanivasakan/documents/github/ogdatathonnov2020/chromedriver'
 driver = webdriver.Chrome(path)
-profile = 'https://www.guidestar.org/profile/11-1633549'
-driver.get(profile)
 
-page_info = []
+#
+all_orgs = []
 
-name = driver.find_element_by_class_name('profile-org-name')
+# for loop for i in list from df
+# set profile = i
+data_provided = [INSERT DATA FRAME HERE COLUMNS OF WEBSITE]
 
-each = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'report-section')))
-element = driver.find_elements_by_class_name('report-section')
+for i in data_provided:
 
-for i in element:
-    if i.find_element_by_class_name('report-section-header') == 'Ruling year':
-        value = i.find_element_by_class_name('report-section-text')
-        year = value.get_attribute('p') 
-    elif i.find_element_by_class_name('report-section-header') == 'President' or element.find_element_by_class_class_name('report-section-header') == 'Principle Officer':
-        value = i.find_element_by_class_name('report-section-text')
-        principle_officer = value.get_attribute('p')
-    elif i.find_element_by_class_name('report-section-header') == 'SIC Code':
-        value = i.find_element_by_class_name('report-section-text mb-2')
-        sector_area = value.get_attribute('p')
-    elif i.find_element_by_class_name('report-section-header') == 'Subject area':
-        subject_area = i.find_element_by_class_name('report-section-text mb-2')
-    elif i.find_element_by_class_name('report-section-header') == 'Population served':
-        target_audience = i.find_element_by_class_name('report-section-text mb-2')
-    org_type = 'non_profit'
+    profile = 'https://www.guidestar.org/profile/11-1633549'
+    driver.get(profile)
 
-mission = driver.find_element_by_id("mission")
-element = driver.find_element_by_class_name('hide-print-url')
-website = element.get_attribute('href') # check to see if this get's fucked up
+    name = driver.find_element_by_class_name('profile-org-name')
 
-num_employees= driver.find_element_by_id("mission")
-services= driver.find_element_by_id("mission")
-description= driver.find_element_by_id("mission")
-reach= driver.find_element_by_id("mission")
-org_size= driver.find_element_by_id("mission")
-size_of_audience= driver.find_element_by_id("mission")
+    each = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'report-section')))
+    element = driver.find_elements_by_class_name('report-section')
 
-element = driver.find_element_by_id('keywords')
-values = element.find_element_by_id('keyword')
-keywords = values.get_attribute('p')
+    for i in element:
+        if i.find_element_by_class_name('report-section-header') == 'Ruling year':
+            value = i.find_element_by_class_name('report-section-text')
+            year = value.get_attribute('p') 
+        elif i.find_element_by_class_name('report-section-header') == 'President' or element.find_element_by_class_class_name('report-section-header') == 'Principle Officer':
+            value = i.find_element_by_class_name('report-section-text')
+            principle_officer = value.get_attribute('p')
+        elif i.find_element_by_class_name('report-section-header') == 'SIC Code':
+            value = i.find_element_by_class_name('report-section-text mb-2')
+            sector_area = value.get_attribute('p')
+        elif i.find_element_by_class_name('report-section-header') == 'Subject area':
+            subject_area = i.find_element_by_class_name('report-section-text mb-2')
+        elif i.find_element_by_class_name('report-section-header') == 'Population served':
+            target_audience = i.find_element_by_class_name('report-section-text mb-2')
+        org_type = 'non_profit'
 
-page_info.append({'name': name, 'year': year, 'principle_officer': principle_officer,
-'mission': mission, 'subject_area':subject_area, 'sector_area':sector_area, 'website':website, 
-'num_employees':num_employees, 'org_type':org_type, 'services':services, 'keywords': keywords, 
-'description':description, 'target_audience':target_audience,
-'reach':reach, 'org_size':org_size, 'size_of_audience':size_of_audience})
+    mission = driver.find_element_by_id("mission")
+    element = driver.find_element_by_class_name('hide-print-url')
+    website = element.get_attribute('href') # check to see if this get's fucked up
 
-print(page_info)
+    num_employees= driver.find_element_by_id("mission")
+    services= driver.find_element_by_id("mission")
+    description= driver.find_element_by_id("mission")
+    reach= driver.find_element_by_id("mission")
+    org_size= driver.find_element_by_id("mission")
+    size_of_audience= driver.find_element_by_id("mission")
+
+    element = driver.find_element_by_id('keywords')
+    values = element.find_element_by_id('keyword')
+    keywords = values.get_attribute('p')
+
+    all_orgs.append({'name': name, 'year': year, 'principle_officer': principle_officer,
+    'mission': mission, 'subject_area':subject_area, 'sector_area':sector_area, 'website':website, 
+    'num_employees':num_employees, 'org_type':org_type, 'services':services, 'keywords': keywords, 
+    'description':description, 'target_audience':target_audience,
+    'reach':reach, 'org_size':org_size, 'size_of_audience':size_of_audience})
+
 
